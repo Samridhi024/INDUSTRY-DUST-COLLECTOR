0dfa966b01bd038d381fd91cdc39e0b0fd6454e1
Samridhi Sinha <162716314+Samridhi024@users.noreply.github.com>
2025-06-21 19:16:21 +0530

    Update authContext.js

--- Dashboard(Frontend)/src/authContext.js
+++ Dashboard(Frontend)/src/authContext.js
@@ -15,7 +15,7 @@ export const AuthProvider = ({ children }) => {
 
     const login = async (email, password) => {
         try {
-            const res = await axios.post("http://industry-dust-monitor-production-a405.up.railway.app/api/login", { email, password });
+            const res = await axios.post("http://industry-dust-monitor-production-a405.up.railway.app/login", { email, password });
             setToken(res.data.token);
             localStorage.setItem("token", res.data.token);
             setUser(res.data.user);
